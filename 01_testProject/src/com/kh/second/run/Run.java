8324f21f1f8aa372ebf1711de714305041b9a5c0
Yeni0325 <88079979+Yeni0325@users.noreply.github.com>
2021-07-30 11:42:23 +0900

    Add files via upload

---
+++ 01_testProject/src/com/kh/second/run/Run.java
@@ -0,0 +1,12 @@
+package com.kh.second.run;
+
+import com.kh.second.MyName;
+;
+public class Run {
+
+	public static void main(String[] args) {
+		
+		MyName mn = new MyName(); // mn이라는 객체 생성
+		mn.callMyName();
+	}
+}
