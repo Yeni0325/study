c4a5cdef52a1fa7b91685340da73f6ee584c5446
Yeni0325 <88079979+Yeni0325@users.noreply.github.com>
2021-08-02 15:25:57 +0900

    A_Variable / B_Keyboardinput / C_Printf 클래스 실행

---
+++ 02_Variable/Run.java
@@ -0,0 +1,33 @@
+package com.kh.run;
+
+import com.kh.variable.A_Variable;
+import com.kh.variable.B_KeyboardInput;
+import com.kh.variable.C_Printf;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		A_Variable a = new A_Variable();
+		// a.printVariable();
+		
+		//a.declareVariable();
+		
+		B_KeyboardInput b = new B_KeyboardInput();
+		
+		//b.inputTest1();
+		
+		//b.inputTest2();
+		
+		//b.inputTest3();
+		
+		//b.inputTest4();
+		
+		//b.charAtTest();
+		
+		C_Printf c = new C_Printf();
+		
+		c.printfTest();
+
+	}
+}
