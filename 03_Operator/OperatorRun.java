cb1fd81c2baa6daad309cfbf2a1582d44453d894
Yeni0325 <88079979+Yeni0325@users.noreply.github.com>
2021-08-03 18:18:40 +0900

    A_LogicalNegation 클래스 실행

---
+++ 03_Operator/OperatorRun.java
@@ -0,0 +1,15 @@
+package com.kh.run;
+
+import com.kh.operator.A_LogicalNegation;
+
+public class OperatorRun {
+	
+	public static void main(String[] args) {
+		
+		//System.out.println("잘실행되나?");
+		
+		A_LogicalNegation a = new A_LogicalNegation();
+		
+		a.method1();
+	}
+}
