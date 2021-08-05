f76cd102aee93afdbe3effa3f7cd0968e8ff6a92
Yeni0325 <88079979+Yeni0325@users.noreply.github.com>
2021-08-05 17:11:00 +0900

    연산자 실습문제 풀이

---
+++ 03_Operator/practice/run/Run.java
@@ -0,0 +1,28 @@
+package com.kh.practice.run;
+import com.kh.practice.example.OperatorPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		
+	OperatorPractice a = new OperatorPractice();
+	
+	//a.practice1();
+	
+	//a.practice2();
+	
+	//a.practice3();
+
+	//a.practice4();
+	
+	//a.practice5();
+	
+	//a.practice6();
+	
+	//a.practice7();
+	
+	a.practice8();
+	}
+
+}
