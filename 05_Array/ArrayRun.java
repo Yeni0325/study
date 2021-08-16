e1de67ad460378c383b32be76b662fefbf897894
Yeni0325 <88079979+Yeni0325@users.noreply.github.com>
2021-08-16 19:08:18 +0900

    A_Array 클래스를 실행하는 메인메소드

--- 05_Array/ArrayRun.java
+++ 05_Array/ArrayRun.java
@@ -9,6 +9,14 @@ public class ArrayRun {
 		A_Array a = new A_Array();
 		
 		//a.method1();
-		a.method2();
+		//a.method2();
+		//a.method3();
+		//a.method4();
+		//a.method5();
+		//a.method6();
+		//a.method7();
+		//a.method8();
+		//a.method9();
+		a.method10();
 	}
 }
